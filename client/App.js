df16694aba00821353b4372549069ecdf8c8ce8f
GIRISHGHUGARE <girishghugare19@gmail.com>
2024-10-30 19:51:32 +0530

    Welcome Screen added

--- client/App.js
+++ client/App.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { LittleLemonHeader } from './components/LittleLemonHeader.js';
 import { LittleLemonFooter } from './components/LittleLemonFooter.js';
 
@@ -11,8 +11,25 @@ export default function App() {
           backgroundColor: '#495E57',
         }}>
         <LittleLemonHeader />
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          margin: 10,
+          fontSize: 38,
+        }}>
+          Welcome to Little Lemon
+        </Text>
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          margin: 20,
+          fontSize: 28,
+        }}>
+          Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. 
+          We would love to hear more about your experience with us!
+        </Text>
       </View>
-      
+
       <View style={{ backgroundColor: '#495E57' }}>
         <LittleLemonFooter />
       </View>
